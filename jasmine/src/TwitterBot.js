e2405e2978922e1380007e940d64fb3119525b65
Stuart Cooper <stucooper86@gmail.com>
2016-11-22 16:34:26 +0000

    updated weather bot

--- jasmine/src/TwitterBot.js
+++ jasmine/src/TwitterBot.js
@@ -8,15 +8,18 @@ twitterBot.stream('statuses/filter', {track: '@makers_twit_bot'}, function(strea
 
   stream.on('data', function(tweet) {
     var splitArray = tweet.text.replace(/\?/g,'').split(' ');
-    var cities = ['London', 'Vancouver', 'Montevideo', 'Havana', 'Dublin', 'Brasilia', 'Nicosia', 'Paris', 'Miami', 'Sydney', 'Tokyo', 'Madrid', 'Moscow', 'Beijing', 'Mumbai', 'Cairo', 'Manchester', 'Timbuktu', 'Auckland'];
-    var arr = splitArray.concat(cities);
-    var sortedArr = arr.sort();
-    var result = [];
-    for (var i = 0; i < arr.length - 1; i++) {
-      if (sortedArr[i + 1] == sortedArr[i]) {
-        result.push(sortedArr[i]);
+    if (splitArray.includes('weather') || splitArray.includes('temperature')) {
+      var cities = ['London', 'Vancouver', 'Montevideo', 'Havana', 'Dublin', 'Brasilia', 'Nicosia', 'Paris', 'Miami', 'Sydney', 'Tokyo', 'Madrid', 'Moscow', 'Beijing', 'Mumbai', 'Cairo', 'Manchester', 'Timbuktu', 'Auckland'];
+      var arr = splitArray.concat(cities);
+      var sortedArr = arr.sort();
+      var result = [];
+      for (var i = 0; i < arr.length - 1; i++) {
+        if (sortedArr[i + 1] == sortedArr[i]) {
+          result.push(sortedArr[i]);
+        }
       }
-     }
+
+
 
     var city = result[0];
     request("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=134d393fd68fd2c3c4db178a50b5ddb8", function(error2,response2,body2) {
@@ -32,8 +35,8 @@ twitterBot.stream('statuses/filter', {track: '@makers_twit_bot'}, function(strea
         console.log(tweetReply.text);
       });
     });
-  });
-
+  }
+});
   // ... when we get an error...
   stream.on('error', function(error) {
     //print out the error
