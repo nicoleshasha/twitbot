70ab5f1e3ea708f671de1ec2d2c841776f9039ff
Dionysis <Dionysis.k22@gmail.com>
2016-11-22 12:36:14 +0000

    renamed file to twitterbot in src of jasmine folder, request to api

---
+++ jasmine/spec/appSpec.js
@@ -0,0 +1,18 @@
+// require(./app.js)
+//
+// describe("Twitter", function() {
+//   var twitter;
+//
+//   beforeEach(function() {
+//     twitter = new TwitterPackage();
+//   });
+//
+//   it("listens to tweets", function() {
+//     twitter.stream('statuses/filter', {track: '#food'}, function(stream) {
+//       stream.on('data', function(tweet) {
+//         console.log(tweet)
+//       expect(tweet.text).toContain(#food)
+//       })
+//     });
+//   });
+// });
---
+++ jasmine/src/TwitterBot.js
@@ -0,0 +1,53 @@
+var TwitterPackage = require('twitter');
+var secret = require("../secret");
+var request = require("request")
+
+
+//make a new Twitter object
+var twitterBot = new TwitterPackage(secret);
+
+// WEATHER
+// Call the stream function and pass in 'statuses/filter', our filter object, and our callback
+twitterBot.stream('statuses/filter', {track: '#food'}, function(stream) {
+  // ... when we get tweet data...
+  stream.on('data', function(tweet) {
+
+    // print out the text of the tweet that came in
+    console.log(tweet.text);
+    console.log(tweet.user.screen_name);
+
+    // url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=134d393fd68fd2c3c4db178a50b5ddb8"
+
+    request("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=134d393fd68fd2c3c4db178a50b5ddb8", function(error2,response2,body2) {
+      console.log("hi!!!!! iiiiii !!!!!!!")
+      console.log("        iiiiii        ")
+      console.log("hi!!!!! iiiiii !!!!!!!")
+
+      console.log(body2)
+      console.log(body2.id)
+      console.log(body2.main)
+      console.log(body2.weather)
+    });
+
+
+    //build our reply object
+    var statusObj = {status: "Hi @" + tweet.user.screen_name + ",the temperature is"+ weather+"'C"}
+    //call the post function to tweet something
+    // twitterBot.post('statuses/update', statusObj,  function(error, tweetReply, response){
+    //
+    //   //if we get an error print it out
+    //   if(error){
+    //     console.log(error);
+    //   }
+    //
+    //   //print the text of the tweet we sent out
+    //   console.log(tweetReply.text);
+    // });
+  });
+
+  // ... when we get an error...
+  stream.on('error', function(error) {
+    //print out the error
+    console.log(error);
+  });
+});
