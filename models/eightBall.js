7cfb7787e2aae3a3685c88e9978441e0bc96d520
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-22 14:59:38 +0000

    adds working 8 ball test

--- models/eightBall.js
+++ models/eightBall.js
@@ -1,18 +1,9 @@
 function EightBall() {
 
-var array = ["Yes", "No", "Maybe", "Possibly", "Definitely", "Not recommended", "Reconsider", "Are you sure?", "Potentially", "Absolutely"]
+  this.responses = ["Yes", "No", "Maybe", "Possibly", "Definitely", "Not recommended", "Reconsider", "Are you sure?", "Potentially", "Absolutely"]
 
-EightBall.prototype.answer = function () {
-  return array[Math.floor(Math.random() * array.length)];
 };
 
-EightBall.prototype.arrayIterator = function (answer) {
-    var array = ["Yes", "No", "Maybe", "Possibly", "Definitely", "Not recommended", "Reconsider", "Are you sure?", "Potentially", "Absolutely"]
-    for (i = 0; i == array.length; i++){
-      if(answer == array[i]) {
-        console.log(array[i])
-        return true;
-        }
-      }
-    }
+EightBall.prototype.answer = function () {
+  return this.responses[Math.floor(Math.random() * this.responses.length)];
 };
--- spec/eightBallSpec.js
+++ spec/eightBallSpec.js
@@ -2,14 +2,8 @@ describe("EightBall", function() {
 
 var eightBall = new EightBall();
 
-  // it("can be asked a question", function(){
-  //   eightBall.input();
-  //   expect(eightBall.input).to
-  // });
-console.log(eightBall.answer());
-
   it("responds with an answer to a question", function(){
-    expect(eightBall.arrayIterator(eightBall.answer)).toBeTruthy();
+    expect(eightBall.responses).toContain(eightBall.answer());
   })
 
 });
