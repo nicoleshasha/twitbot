a1a88bf55548b3b86ba67906e7aa516fb01af4ea
Stuart Cooper <stucooper86@gmail.com>
2016-11-22 16:26:20 +0000

    adding working weather bot

--- jasmine/src/TwitterBot.js
+++ jasmine/src/TwitterBot.js
@@ -1,41 +1,37 @@
 var TwitterPackage = require('twitter');
 var secret = require("../secret");
-var request = require("request")
+var request = require("request");
 
-
-//make a new Twitter object
 var twitterBot = new TwitterPackage(secret);
 
-// WEATHER
-// Call the stream function and pass in 'statuses/filter', our filter object, and our callback
-twitterBot.stream('statuses/filter', {track: '#hippopotomus'}, function(stream) {
-  // ... when we get tweet data...
+twitterBot.stream('statuses/filter', {track: '@makers_twit_bot'}, function(stream) {
+
   stream.on('data', function(tweet) {
+    var splitArray = tweet.text.replace(/\?/g,'').split(' ');
+    var cities = ['London', 'Vancouver', 'Montevideo', 'Havana', 'Dublin', 'Brasilia', 'Nicosia', 'Paris', 'Miami', 'Sydney', 'Tokyo', 'Madrid', 'Moscow', 'Beijing', 'Mumbai', 'Cairo', 'Manchester', 'Timbuktu', 'Auckland'];
+    var arr = splitArray.concat(cities);
+    var sortedArr = arr.sort();
+    var result = [];
+    for (var i = 0; i < arr.length - 1; i++) {
+      if (sortedArr[i + 1] == sortedArr[i]) {
+        result.push(sortedArr[i]);
+      }
+     }
 
-    // print out the text of the tweet that came in
-    console.log(tweet.text);
-    console.log(tweet.user.screen_name);
-
-    //build our reply object
-    request("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=134d393fd68fd2c3c4db178a50b5ddb8", function(error2,response2,body2) {
+    var city = result[0];
+    request("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=134d393fd68fd2c3c4db178a50b5ddb8", function(error2,response2,body2) {
       weather = JSON.parse(body2);
       tempNum = weather.main.temp;
       tempString = tempNum.toString();
 
-      var statusObj = {status: "Hi @" + tweet.user.screen_name + ", the temperature in London is "+ tempString+"°C"}
+      var statusObj = {status: "Hi @" + tweet.user.screen_name + ", the temperature in " + city + " is "+ tempString+"°C"};
       twitterBot.post('statuses/update', statusObj,  function(error, tweetReply, response){
-
-        //if we get an error print it out
         if(error){
           console.log(error);
         }
-
-        //print the text of the tweet we sent out
         console.log(tweetReply.text);
       });
-
     });
-
   });
 
   // ... when we get an error...
