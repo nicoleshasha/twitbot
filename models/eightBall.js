ef13c2005907533e1d86aae48ff5cdf6904663db
Nicole <nicoleshasha@hotmail.co.uk>
2016-11-22 14:19:01 +0000

    Add array of answers, not working test

--- models/eightBall.js
+++ models/eightBall.js
@@ -1,3 +1,18 @@
-function eightBall() {
+function EightBall() {
 
+var array = ["Yes", "No", "Maybe", "Possibly", "Definitely", "Not recommended", "Reconsider", "Are you sure?", "Potentially", "Absolutely"]
+
+EightBall.prototype.answer = function () {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+EightBall.prototype.arrayIterator = function (answer) {
+    var array = ["Yes", "No", "Maybe", "Possibly", "Definitely", "Not recommended", "Reconsider", "Are you sure?", "Potentially", "Absolutely"]
+    for (i = 0; i == array.length; i++){
+      if(answer == array[i]) {
+        console.log(array[i])
+        return true;
+        }
+      }
+    }
 };
--- spec/eightBallSpec.js
+++ spec/eightBallSpec.js
@@ -1,3 +1,15 @@
-describe("eightBall", function() {
+describe("EightBall", function() {
+
+var eightBall = new EightBall();
+
+  // it("can be asked a question", function(){
+  //   eightBall.input();
+  //   expect(eightBall.input).to
+  // });
+console.log(eightBall.answer());
+
+  it("responds with an answer to a question", function(){
+    expect(eightBall.arrayIterator(eightBall.answer)).toBeTruthy();
+  })
 
 });
