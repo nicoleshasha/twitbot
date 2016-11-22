09f151faae7685146d0694528ee3cbb9255496cb
Nicole <nicoleshasha@hotmail.co.uk>
2016-11-22 10:37:42 +0000

    Comment out tweets

--- jasmine/app.js
+++ jasmine/app.js
@@ -5,8 +5,8 @@ var secret = require("./secret");
 var Twitter = new TwitterPackage(secret);
 
 // Call the stream function and pass in 'statuses/filter', our filter object, and our callback
-Twitter.stream('statuses/filter', {track: '#london'}, function(stream) {
-
+Twitter.stream('statuses/filter', {track: ''}, function(stream) {
+//Twitter.stream('statuses/filter', {track: '#london'}, function(stream) {
   // ... when we get tweet data...
   stream.on('data', function(tweet) {
 
@@ -14,8 +14,8 @@ Twitter.stream('statuses/filter', {track: '#london'}, function(stream) {
     console.log(tweet.text);
 
     //build our reply object
-    var statusObj = {status: "Hi @" + tweet.user.screen_name + " https://www.youtube.com/watch?v=8Pr-p8tCruw"}
-
+    var statusObj = {status: ""}
+    // var statusObj = {status: "Hi @" + tweet.user.screen_name + " https://www.youtube.com/watch?v=8Pr-p8tCruw"}
     //call the post function to tweet something
     Twitter.post('statuses/update', statusObj,  function(error, tweetReply, response){
 
