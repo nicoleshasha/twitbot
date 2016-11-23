f0f6dbc4ffb2e33e16349b2013a7efdf639d96bc
RachaelMahon <rachmahon@gmail.com>
2016-11-23 14:20:32 +0000

    Deployment folders inside master

---
+++ index.js
@@ -0,0 +1,60 @@
+var TwitterPackage = require('twitter');
+var secret = require("./secret");
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/eliza', function(request, response) {
+  response.render('pages/eliza');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+//make a new Twitter object
+var Twitter = new TwitterPackage(secret);
+
+// Call the stream function and pass in 'statuses/filter', our filter object, and our callback
+Twitter.stream('statuses/filter', {track: '#owl'}, function(stream) {
+
+  // ... when we get tweet data...
+  stream.on('data', function(tweet) {
+
+    // print out the text of the tweet that came in
+    console.log(tweet.text);
+
+    //build our reply object
+    var statusObj = {status: "Hi there, I love owls and javascript!"}
+
+    //call the post function to tweet something
+    Twitter.post('statuses/update', statusObj,  function(error, tweetReply, response){
+
+      //if we get an error print it out
+      if(error){
+        console.log(error);
+      }
+
+      //print the text of the tweet we sent out
+      console.log(tweetReply.text);
+    });
+  });
+
+  // ... when we get an error...
+  stream.on('error', function(error) {
+    //print out the error
+    console.log(error);
+  });
+});
