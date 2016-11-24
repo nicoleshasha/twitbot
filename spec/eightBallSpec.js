171436b4c784bfe8d7928748e3706bde50c94254
Nicole <nicoleshasha@hotmail.co.uk>
2016-11-24 15:19:25 +0000

    Refactor test names

--- spec/eightBallSpec.js
+++ spec/eightBallSpec.js
@@ -4,20 +4,20 @@ describe("eightBall", function() {
     this.responses = ["Yes", "No", "Maybe", "Possibly", "Definitely", "Not recommended", "Reconsider", "Are you sure?", "Potentially", "Absolutely"]
   }
 
-  EightBall.prototype.answer = function (username) {
+  EightBall.prototype.eightBall = function (username) {
     return "Hi @" + username + " " + this.responses[Math.floor(Math.random() * this.responses.length)];
  };
 
  beforeEach(function(){
-   eightBall = new EightBall();
+   Magic = new EightBall();
  })
 
  it("responds with an answer to a question", function(){
-   expect(eightBall.responses).toContain("Yes")
+   expect(Magic.responses).toContain("Yes")
  })
 
  it("responds with the username of the person that tweeted it", function(){
-   expect(eightBall.answer("NicoleShasha")).toContain("Hi @NicoleShasha")
+   expect(Magic.eightBall("NicoleShasha")).toContain("Hi @NicoleShasha")
  })
 
 });
